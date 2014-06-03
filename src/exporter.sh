8728cbfd174d3c1059ba328f3f40d987449edc6b
humanchimp <morphcham@gmail.com>
2014-06-03 13:41:35 -0700

    remove weird reassignment

--- src/exporter.sh
+++ src/exporter.sh
@@ -9,6 +9,5 @@ echo "KD.classes = {}"
 
 grep "module.exports\s*=\s*class " * -R | awk -F':' '{ print $1,$2 }' | awk '{ print "KD.classes."$5,"= require \"./"$1"\" " }'
 
-echo "KD = require './core/kd.coffee'"
 echo "KD.exportKDFramework()"
 echo "console.timeEnd 'Framework loaded'"

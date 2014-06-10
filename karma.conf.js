e0914410b3c0eb2be562e4f5a34a5f4fda7adfaf
Umut Sirin <umutsirin1@gmail.com>
2014-06-09 18:48:40 -0700

    test: Add karma-spec-reporter for better tests displaying.

--- karma.conf.js
+++ karma.conf.js
@@ -57,21 +57,21 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
-      
+
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec'],
 
 
     // web server port

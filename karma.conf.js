ccc624bc9bdc5c62ac2734d19a793174bff4de78
Umut Sirin <umutsirin1@gmail.com>
2014-07-11 02:13:10 -0700

    test: add kd.test to karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/kd.libs.js'
+      'test/kd.libs.js',
+      'test/kd.test.js'
     ],
 
     sauceLabs: {
